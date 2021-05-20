c555f2ece2e1cf5e502909eb0ed031b1e4b9fc34
Kazım Nihat Tekiner <nihattekiner@gmail.com>
2021-05-20 10:23:30 +0300

    First Commit

---
+++ bolum1_giris/app.js
@@ -0,0 +1,13 @@
+alert("BUNU ONAYLAMADAN SAYFANIN HIC BIR KISMINI GOREMEZ VE INCELEYEMEZSIN");
+//? Alert yazdigimizda bu alert'i onaylamadan sayfaya erisemiyoruz. 
+//? En basa koyduk cunku once bu onaylansin daha sonra sayfa goruntulenebilsin diye. (Okuma sirasi)
+
+console.log("Hello Javascript");
+//? console.log("Hello Javascript"); Yazdigimiz bu kisim sitemizde neden gozukmedi ? 
+//? Biz bunu HTML sayfasina yazdirmadik sadece "Console"a yazdirdik.
+//? Sayfamiza gidip sag klik yapip incele diyelim, Console kisminda solda User Messages kisminda gozukuyor.
+console.log("JS Java'dan kolay bir dildir.");
+console.error("Bu bir hatadir");
+//? Error ile Messages kisminda error verdiriyoruz. Kirmizi renkle gosterilir.
+console.warn("Bu bir uyaridir.")
+//? Warning ile Uyari veridiriyoruz. Sari renkle gosterilir.
