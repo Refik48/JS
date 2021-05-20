bfe82ec9570ede15377bfd190b64da74f9030773
Kazım Nihat Tekiner <nihattekiner@gmail.com>
2021-05-20 10:52:26 +0300

    19 May 21

--- bolum1_giris/app.js
+++ bolum1_giris/app.js
@@ -11,3 +11,47 @@ console.error("Bu bir hatadir");
 //? Error ile Messages kisminda error verdiriyoruz. Kirmizi renkle gosterilir.
 console.warn("Bu bir uyaridir.")
 //? Warning ile Uyari veridiriyoruz. Sari renkle gosterilir.
+
+//? Site icerisinde incele kisminda Console'a bir seyler yazip buraya aktarabiliyoruz.(Simdi ogrenmeyecegiz)
+
+
+//* ============================= DEGISKEN TANIMLAMA =============================
+//? VAR keyword'u ile degisken tanimlanabilir. Ancak, modern JS ile "var" keyword'unun kullanimi azaldi.
+//? Cunku "var" global degisken gibi dusunulmektedir. 
+
+var pi = 3.14;
+console.log(pi);
+console.log(typeof pi);
+//? Biz pi'nin veri tipini tanimlamamistik fakat kendisi otomatik olarak number tipiyle eslestirdi. 
+//? Bunu da console.log(typeof pi); ile gorebiliyoruz.
+
+pi = "3"; //? Biz nasil yazarsak program ona gore algilayip veri tipini ona gore belirliyor.
+console.log(pi);
+console.log(typeof pi); 
+
+pi = true; //? Biz nasil yazarsak program ona gore algilayip veri tipini ona gore belirliyor.
+console.log(pi);
+console.log(typeof pi); 
+
+isim = "Nihat"; //? Burada degisken tanimlamasi yapmadan yazdim cunku program otomatikmen algiliyor.
+//? En guzeli yazmaktir cunku kodlara bakan baskalari icin daha rahat okuma soz konusudur.
+console.log(isim); //? Mouse ile buranin ustunde durunca "var" degisken tanimlamasi gozukuyor.
+console.log(typeof isim);
+
+isim = 4;
+console.log(isim);
+console.log(typeof isim);
+
+//? Biz const ve let yeni degisken tanimlama keyword'lerini kullanacagiz.
+//? Const : Sabit (finally) gibi dusunulebilir.
+//? Let : var'in local versiyonu gibi dusunulebilir.
+
+//? Const : Sadece baslangicta deger atilabilir. Sonradan degeri DEGISTIRILEMEZ.
+//! Non-primitive'ler haric yeniden deger atilamaz. NP'lerde yeniden deger atanabilir "const" icin.
+
+const piSayisi = 3.14;
+console.log(piSayisi);
+
+//! piSayisi = 3; 
+//? Error kisminda hata verdi Const degerine atama yapilmis diyor. Yeniden atama yapilamaz.
+//! console.log(piSayisi);
