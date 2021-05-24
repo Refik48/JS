7f8ac94b46b7e282c607f592d4b2f05222e128b2
Kazım Nihat Tekiner <nihattekiner@gmail.com>
2021-05-24 22:56:07 +0300

    Update

--- bolum3_fonksiyonlar/app.js
+++ bolum3_fonksiyonlar/app.js
@@ -81,3 +81,47 @@ console.log(topla(3,10));
 const ciftMi = (sayi) => sayi % 2 == 0 ? "Cift" : "Tek";
 console.log(ciftMi(5));
 console.log(ciftMi(2));
+
+//! Ornek
+const ciftMi = (sayi) => (sayi % 2 == 0 ? "çift" : "tek");
+console.log(ciftMi(5));
+console.log(ciftMi(2));
+
+//! Ornek: Us alma
+const taban = prompt("taban giriniz");
+const us = prompt("us giriniz");
+const usAl = (t, u) => t ** u;
+console.log(usAl(taban, us));
+
+//! Ornek: Menü
+//* Ok fonksiyonunda birden fazla ifade var ise fonksiyonda suslu parantez kullanmalıyız.
+const menu = () => {
+  console.log("===============================");
+  console.log("      JAVASCRIPT DERSI         ");
+  console.log("===============================");
+};
+
+menu();
+
+//! Ornek: Bilgi Ver fonksiyonu
+//* 1.YÖNTEM
+const bilgiVer = (ad, soyad, dogum) => {
+  const bilgi = `Adim ${ad}, Soyadim ${soyad} ve yasim ${2021 - dogum}`;
+  return bilgi;
+};
+
+//* 2.YÖNTEM
+const bilgiVer = (ad, soyad, dogum) =>
+  `Adim ${ad}, Soyadim ${soyad} ve yasim ${2021 - dogum}`;
+
+console.log(bilgiVer("Mehmet", "Cakmak", 1989));
+
+//! ORNEK: Silindirin hacmini hesaplayan bir fonksiyon yaziniz.
+const r = prompt("Yaricapi giriniz:");
+const h = prompt("Yukseklik giriniz:");
+
+const hacimHesapla = (r, h) => Math.PI * r * r * h;
+//? En yakın sayiya yuvarlar.
+console.log("Silindir Hacmi:" + Math.round(hacimHesapla(r, h)));
+//? Virgulden sonra 2 basamak alir.
+console.log("Silindir Hacmi:" + hacimHesapla(r, h).toFixed(2));
